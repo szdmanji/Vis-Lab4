019471a130d416b68a4a1b8d33f998b8d33ab335
Shahzad Manji <shahzadmanji1@gmail.com>
2020-10-08 16:44:55 -0500

    some work

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // Lab Four
 
+var countries;
+
+d3.csv('wealth-health-2014.csv', d3.autoType).then(data=> { 
+    console.log('countries', data);
+    countries = data;
+});
+const width = 700;
+const height = 550;
+
+let margin = {top: 20, right: 10, botton: 20, left: 10};
+
+const svg = d3.select('.chart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    
+const xScale = d3
+    .scaleLinear()
+    .domain()
+    .range()
---
+++ test.js
@@ -0,0 +1,42 @@
+
+
+d3.csv('wealth-health-2014.csv', d3.autoType).then(data=> { 
+    console.log('countries', data);
+
+    
+    const margin = {top: 20, right: 10, bottom: 20, left: 10};
+    const width = 400-margin.left-margin.right;
+    const height = 300-margin.top-margin.bottom;
+
+    const svg = d3.select('.chart').append('svg')
+        .attr('width', width+ margin.left+ margin.right)
+        .attr('height', height+ margin.top+ margin.bottom)
+        .append('g')
+        .attr('transform','translate('+margin.left+','+margin.right+')')
+
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d=>d.Income))
+        .range([0, width]);
+
+    const colorScale = d3.scaleSequential(d3.interpolateBlues)
+        .domain(d3.extent(data, d=>d.Income));
+
+    const xAxis = d3.axisBottom(xScale)
+        .ticks(5,'s');
+    
+    svg.append('g')
+        .call(xAxis)
+    svg.selectAll('.income')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('fill', d=>colorScale(d.Income))
+        .attr('class', 'income')
+        .attr('stroke', 'black')
+        .attr('opacity', 0.8)
+        .attr('r', 5)
+        .attr('cx', d=>xScale(d.Income));
+
+    
+});
+
